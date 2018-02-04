7a5c141c42258b5e830bf0f6f9c9043493a6cf56
Allen Ellis <diamondstateellis@gmail.com>
2018-02-04 13:22:16 -0600

    Resized, slowed down turtles, and added a line to form a flower.

--- drawing_squares.py
+++ drawing_squares.py
@@ -16,6 +16,9 @@ def draw_shapes():
     for i in range(1,37):
         draw_triangle(triangle)
         triangle.left(10)
+    triangle.seth(270)
+    triangle.forward(400)
+    
 
     octagon = turtle.Turtle()
     octagon.shape('turtle')
@@ -36,30 +39,30 @@ def draw_shapes():
 def draw_square(square_turtle):
     draw_square = 0
     while draw_square < 4:
-        square_turtle.speed(100)
-        square_turtle.forward(225)
+        square_turtle.speed(10)
+        square_turtle.forward(150)
         square_turtle.right(90)
         draw_square = draw_square + 1
     
 def draw_triangle(triangle_turtle):
-    triangle_turtle.speed(100)
+    triangle_turtle.speed(5)
     make_triangle = 0
     while make_triangle < 3:
-        triangle_turtle.forward(185)
+        triangle_turtle.forward(100)
         triangle_turtle.left(120)
         make_triangle = make_triangle + 1
 
 def draw_octagon(octo_turtle):
     make_oct = 0
-    octo_turtle.speed(350)
+    octo_turtle.speed(10)
     while make_oct < 8:
-        octo_turtle.forward(50)
+        octo_turtle.forward(100)
         octo_turtle.left(45)
         make_oct = make_oct + 1
 
 def draw_circle(circle_turtle):
-    circle_turtle.speed(150)
-    circle_turtle.circle(160)
+    circle_turtle.speed(100)
+    circle_turtle.circle(75)
 
 draw_shapes()        
         
